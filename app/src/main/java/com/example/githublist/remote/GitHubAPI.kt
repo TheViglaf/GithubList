c0a33820d4b1786a5ba545cbb92911cf12a5f43f
TheSchultz <obruno.bbs@gmail.com>
2022-04-03 10:27:15 -0300

    Setup default API manipulation

--- app/src/main/java/com/example/githublist/remote/GitHubAPI.kt
+++ app/src/main/java/com/example/githublist/remote/GitHubAPI.kt
@@ -1,4 +1,14 @@
 package com.example.githublist.remote
 
+import com.example.githublist.remote.responses.Models
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface GitHubAPI {
+    @GET("search/repositories")
+    suspend fun getRepositoriesByStar(
+        @Query("q") queryParams: String,
+        @Query("sort") sort: String = "stars",
+        @Query("page") page: Int
+    ): Models
 }
